56d8f06df310e069e4cf14ae7d10c39afee0c2e2
Chaman Rawat <chamanrawat.dev@gmail.com>
2023-07-23 13:54:42 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const config = require("./utils/config");
+
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+mongoose.set("bufferTimeoutMS", 30000);
+
+logger.info("connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connecting to MongoDB:", error.message);
+  });
+
+app.use(cors());
+app.use(express.static("build"));
+app.use(express.json());
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
